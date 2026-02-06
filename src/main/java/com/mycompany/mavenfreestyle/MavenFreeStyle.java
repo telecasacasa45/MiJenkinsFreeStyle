489c5a26420eb9de6db666aa6eead7715483c5d8
telecasacasa45 <telecasacasa45@gmail.com>
2026-02-06 18:30:12 +0000

    Update MavenFreeStyle.java

--- src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
+++ src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
@@ -15,12 +15,12 @@ public class MavenFreeStyle {
         System.out.println("Hello World!");
     }
     
-    public static int fibonacci(int a){	
+public static int fibonacci(int a){	
 	if (a <= 1){
             return a;
 	} else {
         
-	return fibonacci(a - 1) + fibonacci(a - 2);
+		return fibonacci(a - 1) + fibonacci(a - 2);
     }
 }
 }
