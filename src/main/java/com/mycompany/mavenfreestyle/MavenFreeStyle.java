d03cfeeb613a9315a13fa468cb2fe27514bdef4f
telecasacasa45 <telecasacasa45@gmail.com>
2026-02-06 18:27:27 +0000

    Add files via upload

---
+++ src/main/java/com/mycompany/mavenfreestyle/MavenFreeStyle.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.mavenfreestyle;
+
+/**
+ *
+ * @author olivert
+ */
+public class MavenFreeStyle {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    
+    public static int fibonacci(int a){	
+	if (a <= 1){
+            return a;
+	} else {
+        
+	return fibonacci(a - 1) + fibonacci(a - 2);
+    }
+}
+}
